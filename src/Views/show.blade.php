2d79dda1cfaca8dac997a787518ba6613c6e2795
Francisco Sepulveda <socieboy@gmail.com>
2015-08-05 12:57:21 -0700

    Fixed bug

--- src/Views/show.blade.php
+++ src/Views/show.blade.php
@@ -3,7 +3,7 @@
             swal(
                 {
                     title: "{{ session('sweet_alert_message.title')}}",
-                    text: "{{ session('sweet_alert_message.title')}}",
+                    text: "{{ session('sweet_alert_message.message')}}",
                     type: "{{ session('sweet_alert_message.type')}}",
                     timer: 1500,
                     showConfirmButton: false
@@ -17,7 +17,7 @@
             swal(
                 {
                     title: "{{ session('sweet_alert_message_overlay.title')}}",
-                    text: "{{ session('sweet_alert_message_overlay.title')}}",
+                    text: "{{ session('sweet_alert_message_overlay.message')}}",
                     type: "{{ session('sweet_alert_message_overlay.type')}}",
                     confirmButtonText: "Okay!"
                 }
